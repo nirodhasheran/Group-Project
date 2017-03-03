a17b5892dd3ba8dd6921cae19137171324415a9a
nirodhasheran <sherihewage643@gmail.com>
2017-03-03 16:16:45 +0530

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,202 @@
+<?php 
+include ("config.php");
+
+?>
+<!DOCTYPE html>
+<html>
+
+<title>welcome</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet"  href="style.css">
+   <link rel="stylesheet"  href="font-awesome\css\font-awesome.min.css">
+   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div id="main_container"> 
+
+<nav class="navbar navbar-inverse navbar-fixed-top ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    <a href="#" class="navbar-brand"><b>GANGULA REST</b></a>
+    <button type ="button" class = "navbar-toggle" data-toggle ="collapse" data-target = "#myNavBar">
+      <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+    </div>
+    <div id = "myNavBar" class= "collapse navbar-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php"><i class="glyphicon glyphicon-home"></i><b>Home</b></a></li>
+     <li><a href="aboutus.php"><i class="glyphicon glyphicon-inbox"></i>ABOUT US</a></li>
+    <li><a href="food.php"><i class="glyphicon glyphicon-new-window"></i>FOODMENU</a></li>
+        <li><a href="#"><i class="glyphicon glyphicon-send"></i>EVENT</a></li>
+        <li><a href="GALLERY.php"><i class="glyphicon glyphicon-gift"></i>GALLERY</a></li>
+        <li><a href="BLOG.php"><i class="glyphicon glyphicon-tag"></i>BLOG</a></li>
+         <li><a href="chat.php"><i class="glyphicon glyphicon-tag"></i>LIVE CHAT</a></li>
+        </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="REGISTRATION.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="sign.php"><span class="glyphicon glyphicon-user"></span> LOG IN</a></li>
+        </ul>
+        </div>
+  </div>
+</nav>
+<div class="container1">
+<div class="w3-content w3-display-container">
+  <img class="mySlides" src="q.jpg" style="width:150%;height:50%">
+  <img class="mySlides" src="n.jpg" style="width:100%">
+  <img class="mySlides" src="q.jpg" style="width:100%">
+  <img class="mySlides" src="c.jpg" style="width:100%">
+
+  <a class="w3-btn-floating w3-display-left" onclick="plusDivs(-1)">&#10094;</a>
+  <a class="w3-btn-floating w3-display-right" onclick="plusDivs(1)">&#10095;</a>
+
+
+<script>
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 2500);
+}
+</script>
+</div>
+</div>
+</div>
+
+
+
+
+<div class ="container2">
+<div class = "jumbotron">
+<div class = "row">
+
+<div class ="col-md-4">
+ <a href="food.php"><img  src="q.jpg" style="width:100%"></a>
+Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.
+
+
+
+</div>
+<div class ="col-md-4">
+ <a href="food.php"> <img  src="n.jpg" style="width:100%"></a>
+
+Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.
+Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.
+Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.
+</div>
+<div class ="col-md-4">
+ <a href="food.php"> <img  src="q.jpg" style="width:100%"></a>
+Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.
+Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.
+Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.
+Before you can begin to determine what the composition of a particular paragraph will be, you must first decide on an argument and a working thesis statement for your paper. What is the most important idea that you are trying to convey to your reader? The information in each paragraph must be related to that idea. In other words, your paragraphs should remind your reader that there is a recurrent relationship between your thesis and the information in each paragraph. A working thesis functions like a seed from which your paper, and your ideas, will grow. The whole process is an organic one—a natural progression from a seed to a full-blown paper where there are direct, familial relationships between all of the ideas in the paper.
+</div>
+
+
+
+
+
+
+
+</div>
+
+</div>
+		
+				
+  
+   </div>
+
+
+ 
+
+      
+        
+      
+
+
+   
+ 
+
+
+
+<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>Company<span>logo</span></h3>
+
+				<p class="footer-links">
+					<a href="#">HOME</a>
+					·
+					<a href="#">ABOUT US</a>
+					·
+					<a href="#">FOOD MENU</a>
+					·
+					<a href="#">EVENT</a>
+					·
+					<a href="#">GALLERY</a>
+					·
+					<a href="#">BLOG</a>
+				</p>
+				</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the company</span>
+					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="http://www.facebook.com"><img src="l.jpg" alt="Go to W3Schools!" width="42" height="42" border="0"></a> &nbsp; &nbsp;
+					<a href="http://www.twitter.com"><img src="o.jpg" alt="Go to W3Schools!" width="42" height="42" border="0"></a>  &nbsp; &nbsp;
+					<a href="http://www.googleplus.com"><img src="b.jpg" alt="Go to W3Schools!" width="42" height="42" border="0"></a>  &nbsp; &nbsp;
+					<a href="http://www.linkedin.com"><img src="k.jpg" alt="Go to W3Schools!" width="42" height="42" border="0"></a>  &nbsp; &nbsp;
+
+				</div>
+
+			</div>
+		<div class="footer-right">	
+<div id="map" style="width:180%;height:200px;bottom:-15px; padding: 0px 75px;top:5px;"></div>
+</div>
+<script>
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(7.290572, 80.633728),
+    zoom: 7,
+    zoomControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      position: google.maps.ControlPosition.TOP_CENTER
+    }
+  };
+  var map = new google.maps.Map(mapCanvas ,mapOptions);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
+
+		</footer>
+
+	</body>
+
+</html>
+ 
+ 	
